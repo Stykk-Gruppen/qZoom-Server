e55dbae417f4f78926128e9bf4cf38e22f7ddd49
Tarald <taraldv@gmail.com>
2020-07-17 18:24:11 +0200

    fjernet plugin loader

--- database.cpp
+++ database.cpp
@@ -8,10 +8,6 @@ Database::Database()
 
 bool Database::connectDatabase()
 {
-   // QPluginLoader loader("/usr/local/Qt-5.15.0/plugins/sqldrivers/libqsqlmysql.so");
-    QPluginLoader loader("/home/tarves/Qt/5.15.0/Src/qtbase/plugins/sqldrivers/libqsqlmysql.so");
-    loader.load();
-    loader.errorString();
     db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName(dbHostName);
     db.setDatabaseName(dbDatabaseName);
