5983dd03ed49c6dee8b27efea16b3269aa7c3f33
Tarald <taraldv@gmail.com>
2020-08-09 13:01:40 +0200

    dummy index descrp. in main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,19 @@
 #include "roomshandler.h"
 #include <iostream>
 
+/*! \mainpage My Personal Index Page
+ *
+ * \section intro_sec Introduction
+ *
+ * This is the introduction.
+ *
+ * \section install_sec Installation
+ *
+ * \subsection step1 Step 1: Opening the box
+ *
+ * etc...
+ */
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
